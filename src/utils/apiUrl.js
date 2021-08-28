ee24d13efe88796777eab9e53adab68f61ef27d6
Joe <joedravarol@gmail.com>
2021-08-29 00:23:04 +0100

    fix: Syntax error

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,7 +1,7 @@
 const apiUrl = 'https://meal-picks.herokuapp.com';
 
 export const concatBaseUrl = path => {
-  if (process.env.NODE_ENV && process.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
     return apiUrl + path;
   }
   return path;
