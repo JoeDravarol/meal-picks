32fc7e9dff1f96393249137fbdb336a59e001485
Joe Dravarol <joedravarol@gmail.com>
2022-10-18 23:33:11 +0100

    bugfix: Initialize user favoriteRecipes on refresh
    - Access token is null on initial render after refresh which causes
    an error when making a request to the API

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import MuiTheme from 'components/MuiTheme';
 import AppRoutes from 'routes/AppRoutes';
 import { useAuth } from 'contexts/AuthContext';
 import { initializeFavRecipes } from 'reducers/favoriteRecipeReducer';
+import tokenStorage from 'utils/tokenStorage';
 
 const App = () => {
   const dispatch = useDispatch();
   const auth = useAuth();
 
+  const hasAuthToken = tokenStorage.getConfig().headers.Authorization;
+
   useEffect(() => {
-    if (auth.isAuthenticated()) {
+    if (auth.isAuthenticated() && hasAuthToken) {
       dispatch(initializeFavRecipes());
     }
-  }, [auth, dispatch]);
+  }, [auth, dispatch, hasAuthToken]);
 
   return (
     <MuiTheme>
