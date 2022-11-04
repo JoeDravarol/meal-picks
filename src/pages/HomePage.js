ecbe385c106bf9c73aaadcc50b41cc3dd32f6fdc
Joe Dravarol <joedravarol@gmail.com>
2022-11-04 14:17:27 +0000

    feat: Add about app section to landing page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Hero from 'features/landing/Hero';
+import AboutApp from 'features/landing/AboutApp';
 import Features from 'features/landing/Features';
 import GetStartedBanner from 'features/landing/GetStartedBanner';
 
@@ -8,6 +9,7 @@ const HomePage = () => {
   return (
     <>
       <Hero />
+      <AboutApp />
       <Features />
       <GetStartedBanner />
     </>
