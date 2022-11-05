e1bd09d6b4cc9c4707816dc8e860318270ebc184
Joe Dravarol <joedravarol@gmail.com>
2022-11-05 13:50:41 +0000

    feat: Add sticky footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 import NavBar from 'components/NavBar';
+import Footer, { StickyFooterContainer } from 'components/Footer';
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -18,11 +19,12 @@ const Layout = ({ children }) => {
   const classes = useStyles();
 
   return (
-    <>
+    <StickyFooterContainer>
       <NavBar />
       <div className={classes.toolbar} />
       <main>{children}</main>
-    </>
+      <Footer />
+    </StickyFooterContainer>
   );
 };
 
