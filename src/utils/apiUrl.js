bd69fb54c01ca1f89cbc6ab928cbadf8c130119f
Joe Dravarol <joedravarol@gmail.com>
2022-11-08 13:20:17 +0000

    prod: Updat api url

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://meal-picks.herokuapp.com';
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://drab-tan-cow-wig.cyclic.app/';
+// const BASE_URL = 'http://localhost:3001';
 
 export const concatBaseUrl = path => {
   if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
