5e5d732d962884a764278cd5996a4d6024fee5a3
Joe Dravarol <joedravarol@gmail.com>
2023-12-20 15:24:27 +0000

    fix: API url

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://drab-tan-cow-wig.cyclic.app/';
+const BASE_URL = 'https://drab-tan-cow-wig.cyclic.app';
 // const BASE_URL = 'http://localhost:3001';
 
 export const concatBaseUrl = path => {
